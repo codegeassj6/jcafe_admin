921f0882710ab8b2fd458c4167a0a33d1ca6a2fa
codegeassj6 <jhonreyrepuelaj6@gmail.com>
2023-07-12 14:10:34 +0800

    f

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Game;
 use Illuminate\Http\Request;
+use Validator;
+use Storage;
 
 class GameController extends Controller
 {
@@ -28,7 +30,30 @@ class GameController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|unique:games,name',
+            'genre' => 'string|required',
+            'trailer_link' => 'url|required',
+            'image' => 'file|mimes:png,jpg|required',
+            'rating' => 'integer|required|min:1|max:5'
+        ]);
+
+        if($validator->fails()) {
+          return response()->json(['message' => $validator->messages()->get('*')], 500);
+        }
+
+        Storage::disk('local')->putFileAs('/public/games', $request->file('image'), $request->file('image')->hashName());
+
+        $game = Game::create([
+          'name' => $request->input('name'),
+          'genre' => $request->input('genre'),
+          'trailer_link' => $request->input('trailer_link'),
+          'image' => $request->file('image')->hashName(),
+          'rating' => 4
+        ]);
+
+        return $game;
     }
 
     /**
@@ -52,7 +77,9 @@ class GameController extends Controller
      */
     public function destroy(Request $request)
     {
-        $games = Game::whereIn('id', $request->input('id'))->delete();
-        return response()->json(['message' => 'deleted'], 200);
+
+        Game::whereIn('id', $request->input('id'))->delete();
+
+        return $this->index();
     }
 }
