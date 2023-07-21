3e43ab96a559fc8c24277a280fb8e7b3b4c0f02a
codegeassj6 <jhonreyrepuelaj6@gmail.com>
2023-07-21 20:45:18 +0800

    sadas

--- resources/js/views/Post.vue
+++ resources/js/views/Post.vue
@@ -22,63 +22,29 @@
             class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600"
           >
             <div class="flex pl-0 space-x-1 sm:pl-2">
-              <button
-                type="button"
-                class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
-              >
-                <svg
-                  aria-hidden="true"
-                  class="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+              <div>
+                <button
+                  type="button"
+                  class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+                  @click="initAttachFile"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span class="sr-only">Attach file</span>
-              </button>
-              <button
-                type="button"
-                class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
-              >
-                <svg
-                  aria-hidden="true"
-                  class="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span class="sr-only">Set location</span>
-              </button>
-              <button
-                type="button"
-                class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
-              >
-                <svg
-                  aria-hidden="true"
-                  class="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span class="sr-only">Upload image</span>
-              </button>
+                  <svg
+                    aria-hidden="true"
+                    class="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="sr-only">Attach file</span>
+                </button>
+                <input type="file" ref="attachFiles" class="hidden" @change="morphAttachFiles">
+              </div>
             </div>
             <button
               type="button"
@@ -178,6 +144,7 @@ export default {
         post: {
           attachments: '',
           message: '',
+          attachments: [],
         }
       }
     };
@@ -191,6 +158,15 @@ export default {
   computed: {},
 
   methods: {
+    initAttachFile() {
+      this.$refs.attachFiles.click();
+    },
+
+    morphAttachFiles() {
+      const formData = new FormData;
+      formData.append('files', this.form.post.attachments);
+    },
+
     getPost() {
       const AuthStr = 'Bearer '.concat(userStore().user.access_token);
       axios({
